91f4b3cdd1b1eb74482bb68078042ea0156347ad
Craig Morton <CraigMorton92@gmail.com>
2016-03-25 13:04:20 +0000

    record constructs with album and price

--- record.js
+++ record.js
@@ -1,5 +1,7 @@
-var Record = function(artist){
-  this.artist = artist
+var Record = function(artist, album, price){
+  this.artist = artist.toLowerCase();
+  this.album = album.toLowerCase();
+  this.price = price;
 };
 
 
--- specs/record_spec.js
+++ specs/record_spec.js
@@ -3,9 +3,15 @@ var Record = require("../record");
 
 describe("Record", function(){
   beforeEach(function(){
-    record = new Record("Daft Punk")
+    record = new Record("Daft Punk", "Discovery", 15.99)
   });
   it("should construct with artist attr", function(){
-    assert.equal("Daft Punk", record.artist);
+    assert.equal("daft punk", record.artist);
+  });
+  it("should construct with album", function(){
+    assert.equal("discovery", record.album);
+  });
+  it("should construct with price", function(){
+    assert.equal(15.99, record.price);
   });
 });
