b388e34804ca2b286bc754623b84ec1a4cc8bc8b
Craig Morton <CraigMorton92@gmail.com>
2016-03-25 13:22:20 +0000

    changed test object var names

--- specs/recordStore_spec.js
+++ specs/recordStore_spec.js
@@ -4,16 +4,16 @@ var Record = require("../record");
 
 describe("Record Store", function(){
   beforeEach(function(){
-    store = new RecordStore("Records Records Records Records", "Edinburgh")
+    store1 = new RecordStore("Records Records Records Records", "Edinburgh")
   });
   it("should construct with a name", function(){
-    assert.equal("records records records records", store.name);
+    assert.equal("records records records records", store1.name);
   });
   it("should construct with a city", function(){
-    assert.equal("edinburgh", store.city);
+    assert.equal("edinburgh", store1.city);
   });
   it("should construct with an empty array of records", function(){
-    assert.deepEqual([], store.records);
+    assert.deepEqual([], store1.records);
   });
 
 });
--- specs/record_spec.js
+++ specs/record_spec.js
@@ -3,15 +3,15 @@ var Record = require("../record");
 
 describe("Record", function(){
   beforeEach(function(){
-    record = new Record("Daft Punk", "Discovery", 15.99)
+    record1 = new Record("Daft Punk", "Discovery", 15.99)
   });
   it("should construct with artist attr", function(){
-    assert.equal("daft punk", record.artist);
+    assert.equal("daft punk", record1.artist);
   });
   it("should construct with album", function(){
-    assert.equal("discovery", record.album);
+    assert.equal("discovery", record1.album);
   });
   it("should construct with price", function(){
-    assert.equal(15.99, record.price);
+    assert.equal(15.99, record1.price);
   });
 });
